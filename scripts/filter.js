9262d0e8dff9a4e28c7aac761ad0296e5e96dbad
unnamed42 <a390517426@gmail.com>
2016-09-12 13:35:44 +0800

    Use iteration to generate text-shadow

--- scripts/filter.js
+++ scripts/filter.js
@@ -16,7 +16,7 @@ hexo.extend.filter.register('after_generate',function(){
 hexo.extend.filter.register('after_post_render',function(data){
     
     if(data.content.indexOf("#fn") != -1){
-        data.content += "<script src=\"js/footnote.min.js\" type=\"text/javascript\"></script>";
+        data.content += "<script src=\"/js/footnote.min.js\" type=\"text/javascript\"></script>";
     }
     
     return data;
