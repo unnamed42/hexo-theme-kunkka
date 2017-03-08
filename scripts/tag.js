9b0c9165150716095e5a5d1b089872c727b395b1
unnamed42 <a390517426@gmail.com>
2017-03-08 23:37:59 +0800

    Reimplemented archive page and small css modifications

--- scripts/tag.js
+++ scripts/tag.js
@@ -1,27 +1,20 @@
 'use strict'; 
 
-var escapeHtml = require('hexo-util').escapeHTML;
+var escapeHTML = require('hexo-util').escapeHTML;
 
-hexo.extend.tag.register('sns',function(args){
+hexo.extend.tag.register('sns', function(args) {
     
-    var icon_name = args[0];
+    var icon_name = args[0],
+        content = args[1];
     
-    var content = args[1];
-    
-    var result = '';
+    var result = "";
     
     if(args.length > 2){
-        
-        result = "<a href=\"" + args[2] + "\" class=\"sns fa " + icon_name + "\" target=\"_blank\">";
-        
-        result += escapeHtml(content) + "</a>";
-        
+        result = '<a href="' + args[2] + '" class="sns fa ' + icon_name + '" target="_blank">';
+        result += escapeHTML(content) + "</a>";
     } else {
-        
-        result = "<span class=\"sns fa " + icon_name + "\">";
-        
-        result += escapeHtml(content) + "</span>";
-        
+        result = '<span class="sns fa ' + icon_name + '">';
+        result += escapeHTML(content) + "</span>";
     }
     
     return "<p>" + result + "</p>";
--- source/js/archives.js
+++ source/js/archives.js
@@ -1,57 +1,39 @@
 $(function() {
-    if ($(window).width() > 768) {
-        var top = $("#content").offset().top,
-            year_selected = parseInt($(".year:first").attr("id").replace("year-", ""));
-        $(".year:first, .year .month:first").addClass("selected");
-        $(".month.monthed").click(function() {
+    if($(window).width() <= 768)
+        return;
+    
+    var top = $("#content").offset().top,
+        nav = $("#archive-nav");
+        page_height = $("#primary").position().top + $("#primary").outerHeight(),
+        nav_max = page_height - nav.height();
+    var year_selected = $(".year:first"),
+        month_selected = year_selected.find(".month:first");
+    year_selected.addClass("selected"); month_selected.addClass("selected");
+    $(window).scroll(function() {
+        var scroll_top = $(window).scrollTop();
+        if(scroll_top < 55) // 55 == header.height(55px)
+            nav.css({top: 115 - scroll_top});
+        else if(nav_max > scroll_top)
+            nav.css({top: 60}); // 60 == archive-nav.top(115px) - header.height(55px)
+        else
+            nav.css({top: nav_max - scroll_top});
+        $(".archive-title").each(function() {
             var t = $(this),
-                id = "#" + t.attr("id").replace("mont", "arti");
-            if(!t.hasClass("selected")){
-                // reuse id to store its offset
-                id = $(id).offset().top - 10;
-                $(".month.monthed.selected").removeClass("selected");
-                t.addClass("selected");
-                $("body, html").scrollTop(id);
-            }
-        });
-        $(".year-toogle").click(function(e) {
-            e.preventDefault();
-            var parent = $(this).parent();
-            if (!parent.hasClass("selected")) {
-                var s = parent.children("ul").children("li").eq(0);
-                $(".year.selected").removeClass("selected");
-                parent.addClass("selected");
-                s.click();
-            }
-        });
-        $(window).scroll(function() {
-//             var scroll_top = $(this).scrollTop();
-//             if(scroll_top >= top + 60)
-//                 $(".archive-nav").css({top: 60});
-//             else
-//                 $(".archive-nav").css({top: top + 60 - scroll_top});
-            var scroll_top = $(window).scrollTop();
-            if(scroll_top < 55) // 55 == header.height(55px)
-                $("#archive-nav").css({top: 115 - scroll_top});
-            else
-                $("#archive-nav").css({top: 60}); // 35 == archive-nav.top(115px) - header.height(55px)
-            $(".archive-title").each(function() {
-                var t = $(this),
-                    id = t.attr("id"),
-                    year = parseInt(id.replace(/arti-(\d*)-\d*/, "$1")),
-                    offset = t.offset().top - 40,
-                    bottom = offset + t.height();
-                if(offset <= scroll_top && scroll_top < bottom) {
-                    if(year != year_selected) {
-                        $("#year-" + year_selected).removeClass("selected");
-                        $("#year-" + year).addClass("selected");
-                        year_selected = year;
-                    }
-                    id = "#" + id.replace("arti", "mont");
-                    $(".month.monthed.selected").removeClass("selected");
-                    $(id).addClass("selected");
+                offset = t.offset().top - 40,
+                bottom = offset + t.height();
+            if(offset <= scroll_top && scroll_top < bottom) {
+                var id = t.attr("id"),
+                    year = id.replace(/archive-(\d+)-\d+/, "$1"),
+                    selected_year = year_selected.attr("id").substr(5); // remove "year-" from id
+                if(year !== selected_year) {
+                    year_selected.removeClass("selected");
+                    year_selected = $("#year-" + year);
+                    year_selected.addClass("selected");
                 }
-            });
+                month_selected.removeClass("selected");
+                month_selected = $("#" + id.replace("archive", "month"));
+                month_selected.addClass("selected");
+            }
         });
-    }
+    });
 });
