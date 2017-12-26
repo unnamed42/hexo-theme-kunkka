06a34d1f9b8aa443e635dbc005477b99d25309be
unnamed42 <a390517426@gmail.com>
2017-12-26 12:39:19 +0800

    templatify

--- source/js/disqus.js
+++ source/js/disqus.js
@@ -1,28 +1,29 @@
 $(function() {
-    var comments = $(".dq-recent-comments"),
-        config = {
-            "api_key": CONFIG.disqus.api_key,
-            "forum": CONFIG.disqus.forum,
-            "limit": comments.attr("data-num-items")
-        };
-    if(comments.length) {
-        $.ajax({
-            url: "https://disqus.com/api/3.0/forums/listPosts.jsonp",
-            data: $.param({"limit": config.limit, "forum": config.forum, "related": "thread", "api_key": config.api_key}),
-            dataType: "jsonp",
-            contentType: "text/javascript; charset=utf-8",
-        }).done(function(data) {
-            var html = "";
-            $.each(data.response, function(index, comment){
-                var msg = comment.message.length > 20 ? comment.message.substring(0, 18) + "..." : comment.message;
-                msg = msg.replace(/<\/?p>/g, "");
-                html += '<li>';
-                html +=     '<p><a href="' + comment.thread.link + '#comments" aria-label="于 ' + comment.thread.title + ' 中的评论" class="tooltipped tooltipped-n">' + msg + '</p>';
-                html +=     '<p class="comment-meta"><a rel="nofollow author" target="_blank" href="' + comment.author.profileUrl + '">' + comment.author.name + "</a> ";
-                html +=     '<span class="comment-time" title="' + comment.createdAt + '">' + comment.createdAt + "</span>";
-                html += "</p></li>";
-            });
-            comments.html(html);
+    var comments = $(".dq-recent-comments");
+    $.get({
+        url: "https://disqus.com/api/3.0/forums/listPosts.jsonp",
+        data: $.param({api_key: CONFIG.disqus.api_key, forum: CONFIG.disqus.forum,
+                       limit: comments.attr("data-num-items"), related: "thread"}),
+        dataType: "jsonp",
+        contentType: "text/javascript; charset=utf-8",
+    }).then(function(data) {
+        return $.map(data.response, function(comment) {
+            var msg = comment.message.length > 20 ? comment.message.substring(0, 18) + "..." : comment.message;
+            return {
+                comment: msg.replace(/<\/?p>/g, ""),
+                link: comment.thread.link,
+                title: comment.thread.title,
+                profileUrl: comment.author.profileUrl,
+                name: comment.author.name,
+                createdAt: comment.createdAt
+            };
         });
-    }
+    }).then(function(data) {
+        var template = $("#dq-recent-comments-template").html().split(/\{\{([^\}]+)\}\}/g);
+        comments.append(data.map(function(item) {
+            return template.map(function(tok, i) {
+                return (i % 2) ? item[tok] : tok;
+            }).join('');
+        }));
+    });
 });
