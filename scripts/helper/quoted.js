8dd001277fd52449077703ec83107b0cc8eae6f4
unnamed42 <a390517426@gmail.com>
2018-04-04 02:15:49 +0800

    rename: stringify => quoted

--- scripts/helper/quoted.js
+++ scripts/helper/quoted.js
@@ -1,6 +1,6 @@
 "use strict";
 
-hexo.extend.helper.register("stringify", function(str) {
+hexo.extend.helper.register("quoted", function(str) {
     str = typeof str == "String" ? str : str.toString();
     if(str.charAt(0) == '"' && str.charAt(str.length - 1) == '"')
         return str;
